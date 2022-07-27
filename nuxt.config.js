9ec824c87dcccdd53b04ccb6686930e75046bdc9
talaxasy <talaxasy.or.lev@gmail.com>
2022-07-27 14:33:43 +0300

    finally settings for .vscode

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,11 +4,27 @@ export default {
     title: 'nuxt',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: '',
+      },
+      {
+        name: 'format-detection',
+        content: 'telephone=no',
+      },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/favicon.ico',
+      },
+    ],
   },
 
   srcDir: 'src/',
