a7547450d7401d6ef66d7f1a441f95ecbfc92444
talaxasy <talaxasy.or.lev@gmail.com>
2022-07-27 13:58:59 +0300

    test github CI

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,60 +1,33 @@
 <template>
   <div class="container">
-    <CBox
-      v-bind="mainStyles[colorMode]"
-      d="flex"
-      w="100vw"
-      h="100vh"
-      flex-dir="column"
-      justify-content="center"
-    >
+    <CBox v-bind="mainStyles[colorMode]" d="flex" w="100vw" h="100vh" flex-dir="column" justify-content="center">
       <CHeading text-align="center" mb="4"> ⚡️ Hello chakra-ui/vue </CHeading>
       <CFlex justify="center" direction="column" align="center">
         <CBox mb="3">
-          <CIconButton
-            mr="3"
-            :icon="colorMode === 'light' ? 'moon' : 'sun'"
-            :aria-label="`Switch to ${
-              colorMode === 'light' ? 'dark' : 'light'
-            } mode`"
-            @click="toggleColorMode"
-          />
+          <CIconButton mr="3" :icon="colorMode === 'light' ? 'moon' : 'sun'" :aria-label="`Switch to ${colorMode === 'light' ? 'dark' : 'light'
+          } mode`" @click="toggleColorMode" />
           <CButton left-icon="info" variant-color="blue" @click="showToast">
             Show Toast
           </CButton>
         </CBox>
         <CAvatarGroup>
-          <CAvatar
-            name="Evan You"
-            alt="Evan You"
-            src="https://pbs.twimg.com/profile_images/1206997998900850688/cTXTQiHm_400x400.jpg"
-          >
+          <CAvatar name="Evan You" alt="Evan You"
+            src="https://pbs.twimg.com/profile_images/1206997998900850688/cTXTQiHm_400x400.jpg">
             <CAvatarBadge size="1.0em" bg="green.500" />
           </CAvatar>
-          <CAvatar
-            name="Jonathan Bakebwa"
-            alt="Jonathan Bakebwa"
-            src="https://res.cloudinary.com/xtellar/image/upload/v1572857445/me_zqos4e.jpg"
-          >
+          <CAvatar name="Jonathan Bakebwa" alt="Jonathan Bakebwa"
+            src="https://res.cloudinary.com/xtellar/image/upload/v1572857445/me_zqos4e.jpg">
             <CAvatarBadge size="1.0em" bg="green.500" />
           </CAvatar>
-          <CAvatar
-            name="Segun Adebayo"
-            alt="Segun Adebayo"
-            src="https://pbs.twimg.com/profile_images/1169353373012897802/skPUWd6e_400x400.jpg"
-          >
+          <CAvatar name="Segun Adebayo" alt="Segun Adebayo"
+            src="https://pbs.twimg.com/profile_images/1169353373012897802/skPUWd6e_400x400.jpg">
             <CAvatarBadge size="1.0em" bg="green.500" />
           </CAvatar>
           <CAvatar src="pop">
             <CAvatarBadge size="1.0em" border-color="papayawhip" bg="tomato" />
           </CAvatar>
         </CAvatarGroup>
-        <CButton
-          left-icon="close"
-          variant-color="red"
-          mt="3"
-          @click="showModal = true"
-        >
+        <CButton left-icon="close" variant-color="red" mt="3" @click="showModal = true">
           Delete Account
         </CButton>
         <CModal :is-open="showModal">
@@ -64,11 +37,7 @@
             <CModalBody>Deleting user cannot be undone</CModalBody>
             <CModalFooter>
               <CButton @click="showModal = false"> Cancel </CButton>
-              <CButton
-                margin-left="3"
-                variant-color="red"
-                @click="showModal = false"
-              >
+              <CButton margin-left="3" variant-color="red" @click="showModal = false">
                 Delete User
               </CButton>
             </CModalFooter>
